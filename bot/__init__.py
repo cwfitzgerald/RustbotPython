58afe421197bbdc36700ed88fe941873b6298e3d
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-08-16 21:12:30 -0400

    Changes for nova server

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,18 +13,12 @@ def setup_logging():
         "[{asctime}] [{levelname:<7}] {name}: {message}", dt_fmt, style="{"
     )
 
-    file_handler = logging.FileHandler(
-        filename="logging.log", encoding="utf-8", mode="w"
-    )
-    file_handler.setFormatter(formatter)
-
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
     return logger
